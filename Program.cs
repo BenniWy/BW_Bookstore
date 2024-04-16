98daa63d2ae8c08891f919a31e7aedd128e04ef5
BenniWy <benjamin.wynants@gmx.de>
2024-04-16 17:55:07 +0200

    push login register

--- Program.cs
+++ Program.cs
@@ -9,9 +9,22 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services
+    .AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultUI()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IHomeRepository,HomeRepository>();
+builder.Services.AddTransient<ICartRepository,CartRepository>();
+builder.Services.AddTransient<IUserOrderRepository,UserOrderRepository>();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+   await DbInitializer.SeedDefaultData(scope.ServiceProvider);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -31,5 +44,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
