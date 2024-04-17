7678d6253de6e1ce0c64f0a28d926abb3a34dd9f
BenniWy <benjamin.wynants@gmx.de>
2024-04-17 10:26:06 +0200

    push

--- Models/DTOs/CheckoutModel.cs
+++ Models/DTOs/CheckoutModel.cs
@@ -12,8 +12,10 @@ public class CheckoutModel
     [EmailAddress]
     [MaxLength(30)]
     public string? Email { get; set; }
+
     [Required]
     public string? MobileNumber { get; set; }
+    
     [Required]
     [MaxLength(200)]
     public string? Address { get; set; }
